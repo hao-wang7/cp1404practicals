279cf95e90da51383c54a5086fd172ea66bde2e8
DA <hao.wang7@my.jcu.edu.au>
2025-06-21 16:10:59 +0800

    prac5 emails # Split the username by '.' to separate name parts

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,3 +7,5 @@ Actual:  28 minutes
 def extract_name_from_email(email):
     # Get the part before '@' in the email
     username = email.split('@')[0]
+    # Split the username by '.' to separate name parts
+    parts = username.split('.')
