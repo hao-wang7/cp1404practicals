b4a38b3afb351c0369a7ee71ca49bc326e9dddff
DA <hao.wang7@my.jcu.edu.au>
2025-06-21 17:12:04 +0800

    prac5 word_occurrences # Loop over each word in the list

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -12,3 +12,10 @@ words = text.split()
 
 # Create a dictionary to count occurrences of each word
 word_counts = {}
+
+# Loop over each word in the list
+for word in words:
+    if word in word_counts:
+        word_counts[word] += 1  # Increment count if word already seen
+    else:
+        word_counts[word] = 1   # Initialize count if word is new
