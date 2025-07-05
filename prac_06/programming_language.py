c184086e6c46d9cd4183b0615618b4b986cbe9e4
DA <hao.wang7@my.jcu.edu.au>
2025-07-05 15:37:40 +0800

    Implemented is_dynamic method to check for dynamic typing

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -13,3 +13,6 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dynamic(self):
+        return self.typing.lower() == "dynamic"
