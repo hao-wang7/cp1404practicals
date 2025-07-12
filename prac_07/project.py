53691bd8edf896cb3b7131253d4eb1a327ed0fb9
DA <hao.wang7@my.jcu.edu.au>
2025-07-12 14:49:58 +0800

    Implement comparison and string representation methods for Project

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,3 +8,13 @@ class Project:
         self.priority = int(priority)
         self.cost_estimate = float(cost_estimate)
         self.completion = int(completion)
+
+    def __lt__(self, other):
+
+        return self.priority < other.priority
+
+    def __str__(self):
+
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority {self.priority}, estimate: ${self.cost_estimate:.2f}, "
+                f"completion: {self.completion}%")
