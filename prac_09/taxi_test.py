56f489e6869b93b124e04a5ea8027c439f723d1c
DA <hao.wang7@my.jcu.edu.au>
2025-07-27 15:08:48 +0800

    Print the taxi details and current fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -18,3 +18,10 @@ def main():
 
     # 5. Drive the taxi 100 km
     my_taxi.drive(100)
+
+    # 6. Print the taxi details and current fare
+    print(my_taxi)
+    print("Current fare: ${:.2f}".format(my_taxi.get_fare()))
+
+if __name__ == '__main__':
+    main()
